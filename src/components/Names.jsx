1ef332cee13f2ef3e1e5423e2d47d350eedba30e
hyesungpark <jayhspark@gmail.com>
2021-08-13 10:25:59 +0900

    finish part1

--- src/components/Names.jsx
+++ src/components/Names.jsx
@@ -1,19 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { of } from "rxjs";
+import { of, interval } from "rxjs";
+import { map } from "rxjs/operators";
+import { ajax } from "rxjs/ajax";
+import useObservable from "../hook/useObservable";
 
+const api = `https://randomuser.me/api/?results=5&seed=rx-react&nat=us&inc=name&noinfo`;
+const getName = (user) => `${user.name.first} ${user.name.last}`;
 const source = ["Adam", "Brian", "Christine"];
-const names$ = of(source);
-
+// const names$ = interval(1000).pipe(map((x) => source.slice(0, x + 1)));
+const names$ = ajax
+  .getJSON(api)
+  .pipe(map(({ results: users }) => users.map(getName)));
 const Names = () => {
-  const [names, setNames] = useState(source);
+  const names = useObservable(names$);
 
-  useEffect(() => {
-    const subscription = names$.subscribe(setNames);
-    return () => {
-      console.log("unmount works");
-      subscription.unsubscribe();
-    };
-  }, []);
   return (
     <div>
       <h1>RXJS with React</h1>
